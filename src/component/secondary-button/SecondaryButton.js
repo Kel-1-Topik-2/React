3008ad56a48e2403f0fc9ab5533a19d884a0956c
mikamada <mikamada17@gmail.com>
2022-06-04 20:09:21 +0700

    add component secondary button

---
+++ src/component/secondary-button/SecondaryButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from '@mui/material';
+import styled from '@emotion/styled';
+
+const SecondaryButton = ({ children, onClick }) => {
+	const ButtonSecondary = styled(Button)({
+		height: '56px',
+		width: '206px',
+		color: '#EC0000',
+		fontSize: '18px',
+		backgroundColor: '#EEEEEE',
+	});
+	return (
+		<ButtonSecondary variant="outlined" color="error" onClick={onClick}>
+			{children}
+		</ButtonSecondary>
+	);
+};
+
+export default SecondaryButton;
