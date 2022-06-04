4b78eabf75785c11f57f07b6c432f1f48ef1fcd9
vidinararia <vidinararia@gmail.com>
2022-06-04 14:28:19 +0800

    initialize project

---
+++ src/page/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "../component/footer/Footer";
+import Navbar from "../component/navbar/Navbar";
+
+export default function Homepage() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
