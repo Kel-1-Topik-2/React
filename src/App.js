ec393280becd2498f20716ebaccc014fb9beae5c
vidinararia <vidinararia@gmail.com>
2022-06-08 22:41:59 +0800

    add form pasien

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import Homepage from "./page/Homepage";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import AddData from "./page/form-admin/AddData";
+import DetailData from "./page/form-admin/DetailData";
+import EditData from "./page/form-admin/EditData";
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/AddData" element={<AddData />} />
+        <Route path="/DetailData" element={<DetailData />} />
+        <Route path="/EditData" element={<EditData />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
