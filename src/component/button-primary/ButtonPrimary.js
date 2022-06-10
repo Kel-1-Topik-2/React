e78a6852cebb8794792378ef746d381f44863016
vidinararia <vidinararia@gmail.com>
2022-06-10 17:55:19 +0800

    function tambah data

--- src/component/button-primary/ButtonPrimary.js
+++ src/component/button-primary/ButtonPrimary.js
@@ -1,30 +1,31 @@
-import { Button } from '@mui/material';
+import React from "react";
+import { Button } from "@mui/material";
 
 const ButtonPrimary = ({ title, onClick }) => {
-	// const PrimaryButton = styled(Button)({
-	// 	height: '56px',
-	// 	width: '206px',
-	// 	color: 'white',
-	// 	fontSize: '18px',
-	// 	backgroundColor: '#4CA9EE',
-	// 	borderRadius: '20px',
-	// });
-	return (
-		<Button
-			onClick={onClick}
-			variant="contained"
-			type="submit"
-			sx={{
-				width: '206px',
-				height: '56px',
-				borderRadius: '20px',
-				fontSize: '16px',
-				backgroundColor: '#4CA9EE'
-			}}
-		>
-			<strong>{title}</strong>
-		</Button>
-	);
+  // const PrimaryButton = styled(Button)({
+  // 	height: '56px',
+  // 	width: '206px',
+  // 	color: 'white',
+  // 	fontSize: '18px',
+  // 	backgroundColor: '#4CA9EE',
+  // 	borderRadius: '20px',
+  // });
+  return (
+    <Button
+      onClick={onClick}
+      variant="contained"
+      type="submit"
+      sx={{
+        width: "206px",
+        height: "56px",
+        borderRadius: "20px",
+        fontSize: "16px",
+        backgroundColor: "#4CA9EE",
+      }}
+    >
+      <strong>{title}</strong>
+    </Button>
+  );
 };
 
 export default ButtonPrimary;
