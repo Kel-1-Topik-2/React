4473ee43905492970d3c3e193ba004fb119c4245
mikamada <mikamada17@gmail.com>
2022-06-12 21:59:09 +0700

    fix edit data pasien

--- src/page/form-pasien/EditData.js
+++ src/page/form-pasien/EditData.js
@@ -1,184 +1,247 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import {
-  Box,
-  Button,
-  Grid,
-  IconButton,
-  Paper,
-  Tooltip,
-  Typography,
-  FormControl,
-  RadioGroup,
-  FormLabel,
-  FormControlLabel,
-  Radio,
-} from "@mui/material";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import image from "../../assets/sideFoto/foto.png";
-import FormInput from "../../component/formInput/FormInput";
-import { useNavigate } from "react-router-dom";
+	Box,
+	Button,
+	Grid,
+	IconButton,
+	Paper,
+	Tooltip,
+	Typography,
+	FormControl,
+	RadioGroup,
+	FormLabel,
+	FormControlLabel,
+	Radio,
+} from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import image from '../../assets/sideFoto/foto.png';
+import FormInput from '../../component/formInput/FormInput';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from '../../dummy-api/api';
 
 export default function Form() {
-  let navigate = useNavigate();
+	let navigate = useNavigate();
+	const location = useLocation();
 
-  const handleBack = () => {
-    navigate(-1);
-  };
+	const [editDataPasien, setEditDataPasien] = useState({
+		nama: location.state.nama,
+		nik: location.state.nik,
+		telp: location.state.telp,
+		usia: location.state.usia,
+		alamat: location.state.alamat,
+	});
 
-  const handleCancel = () => {
-    navigate("/");
-  };
+	const [editRadio, setEditRadio] = useState(location.state.jk);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+	const handleChangeRadio = (e) => {
+		setEditRadio(e.target.value);
+	};
 
-  const paperStyle = {
-    padding: "30px 30px",
-    margin: "50px auto",
-    borderRadius: "20px",
-  };
+	const handleChange = (e) => {
+		setEditDataPasien({
+			...editDataPasien,
+			[e.target.name]: e.target.value,
+		});
+	};
 
-  const gridImage = {
-    backgroundImage: `url(${image})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  };
+	const handleBack = () => {
+		navigate(-1);
+	};
 
-  return (
-    <Grid container component="main" sx={{ height: "100vh" }}>
-      <Grid
-        item
-        xs={false}
-        sm={4}
-        md={4}
-        style={gridImage}
-        container
-        direction="column"
-        sx={{ padding: "40px" }}
-      >
-        <Grid item xs>
-          <Tooltip title="back">
-            <IconButton>
-              <ArrowBackIcon
-                sx={{ fontSize: 60, color: "#000000" }}
-                onClick={handleBack}
-              />
-            </IconButton>
-          </Tooltip>
-        </Grid>
-        <Grid>
-          <Typography variant="h3" component="div">
-            <strong>Edit Data Pasien</strong>
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item xs={12} sm={8} md={8} sx={{ backgroundColor: "#EEEEEE" }}>
-        <Box
-          sx={{
-            my: 4,
-            mx: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Paper elevation={4} style={paperStyle}>
-            <Box
-              component="form"
-              sx={{
-                marginTop: 3,
-                marginBottom: 3,
-              }}
-              onSubmit={handleSubmit}
-            >
-              <Grid container spacing={2}>
-                <Grid item xs={7}>
-                  <FormInput title="Nama lengkap*" type="text" />
-                </Grid>
-                <Grid item xs={5}>
-                  <FormInput title="Nomor Handphone*" type="text" />
-                </Grid>
-                <Grid item xs={6}>
-                  <FormInput title="NIK*" type="text" />
-                </Grid>
-                <Grid item xs={2}>
-                  <FormInput title="Usia*" type="number" />
-                </Grid>
-                <Grid item xs={4}>
-                  <FormControl>
-                    <FormLabel
-                      id="demo-row-radio-buttons-group-label"
-                      sx={{ color: "#358C56" }}
-                    >
-                      <strong>Jenis Kelamin</strong>
-                    </FormLabel>
-                    <RadioGroup
-                      row
-                      aria-labelledby="demo-row-radio-buttons-group-label"
-                      name="row-radio-buttons-group"
-                    >
-                      <FormControlLabel
-                        value="Laki-Laki"
-                        control={<Radio />}
-                        label="L"
-                      />
-                      <FormControlLabel
-                        value="Perempuan"
-                        control={<Radio />}
-                        label="P"
-                      />
-                    </RadioGroup>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                  <FormInput
-                    title="Alamat Rumah*"
-                    type="text"
-                    multiline
-                    rows={3}
-                  />
-                </Grid>
-              </Grid>
-              <Grid
-                container
-                spacing={2}
-                sx={{ display: "flex", justifyContent: "center", marginTop: 1 }}
-              >
-                <Grid item>
-                  <Button
-                    variant="outlined"
-                    color="error"
-                    sx={{
-                      width: "160px",
-                      height: "50px",
-                      borderRadius: "20px",
-                      fontSize: "16px",
-                    }}
-                    onClick={handleCancel}
-                  >
-                    <strong>BATAL</strong>
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button
-                    variant="contained"
-                    type="submit"
-                    sx={{
-                      width: "160px",
-                      height: "50px",
-                      borderRadius: "20px",
-                      fontSize: "16px",
-                    }}
-                  >
-                    <strong>Simpan</strong>
-                  </Button>
-                </Grid>
-              </Grid>
-            </Box>
-          </Paper>
-        </Box>
-      </Grid>
-    </Grid>
-  );
+	const handleCancel = () => {
+		navigate('/');
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const newData = {
+			nama: editDataPasien.nama,
+			telp: editDataPasien.telp,
+			nik: editDataPasien.nik,
+			usia: editDataPasien.usia,
+			alamat: editDataPasien.alamat,
+		};
+		axios.put(`/Pasien/${location.state.idPasien}`, newData).then((res) => {
+			console.log(res.data);
+			alert('data telah diubah');
+		});
+	};
+
+	const paperStyle = {
+		padding: '30px 30px',
+		margin: '50px auto',
+		borderRadius: '20px',
+	};
+
+	const gridImage = {
+		backgroundImage: `url(${image})`,
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+	};
+
+	return (
+		<Grid container component="main" sx={{ height: '100vh' }}>
+			<Grid
+				item
+				xs={false}
+				sm={4}
+				md={4}
+				style={gridImage}
+				container
+				direction="column"
+				sx={{ padding: '40px' }}
+			>
+				<Grid item xs>
+					<Tooltip title="back">
+						<IconButton>
+							<ArrowBackIcon
+								sx={{ fontSize: 60, color: '#000000' }}
+								onClick={handleBack}
+							/>
+						</IconButton>
+					</Tooltip>
+				</Grid>
+				<Grid>
+					<Typography variant="h3" component="div">
+						<strong>Edit Data Pasien</strong>
+					</Typography>
+				</Grid>
+			</Grid>
+			<Grid item xs={12} sm={8} md={8} sx={{ backgroundColor: '#EEEEEE' }}>
+				<Box
+					sx={{
+						my: 4,
+						mx: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<Paper elevation={4} style={paperStyle}>
+						<Box
+							component="form"
+							sx={{
+								marginTop: 3,
+								marginBottom: 3,
+							}}
+							onSubmit={handleSubmit}
+						>
+							<Grid container spacing={2}>
+								<Grid item xs={7}>
+									<FormInput
+										title="Nama lengkap*"
+										type="text"
+										name="nama"
+										value={editDataPasien.nama}
+										onChange={handleChange}
+									/>
+								</Grid>
+								<Grid item xs={5}>
+									<FormInput
+										title="Nomor Handphone*"
+										type="text"
+										name="telp"
+										value={editDataPasien.telp}
+										onChange={handleChange}
+									/>
+								</Grid>
+								<Grid item xs={6}>
+									<FormInput
+										title="NIK*"
+										type="text"
+										name="nik"
+										value={editDataPasien.nik}
+										onChange={handleChange}
+									/>
+								</Grid>
+								<Grid item xs={2}>
+									<FormInput
+										title="Usia*"
+										type="number"
+										name="usia"
+										value={editDataPasien.usia}
+										onChange={handleChange}
+									/>
+								</Grid>
+								<Grid item xs={4}>
+									<FormControl>
+										<FormLabel
+											id="demo-row-radio-buttons-group-label"
+											sx={{ color: '#358C56' }}
+										>
+											<strong>Jenis Kelamin</strong>
+										</FormLabel>
+										<RadioGroup
+											row
+											aria-labelledby="demo-row-radio-buttons-group-label"
+											name="row-radio-buttons-group"
+                      value={editRadio}
+                      onChange={handleChangeRadio}
+										>
+											<FormControlLabel
+												value="Laki-Laki"
+												control={<Radio />}
+												label="L"
+											/>
+											<FormControlLabel
+												value="Perempuan"
+												control={<Radio />}
+												label="P"
+											/>
+										</RadioGroup>
+									</FormControl>
+								</Grid>
+								<Grid item xs={12}>
+									<FormInput
+										title="Alamat Rumah*"
+										type="text"
+										multiline
+										rows={3}
+										name="alamat"
+										value={editDataPasien.alamat}
+										onChange={handleChange}
+									/>
+								</Grid>
+							</Grid>
+							<Grid
+								container
+								spacing={2}
+								sx={{ display: 'flex', justifyContent: 'center', marginTop: 1 }}
+							>
+								<Grid item>
+									<Button
+										variant="outlined"
+										color="error"
+										sx={{
+											width: '160px',
+											height: '50px',
+											borderRadius: '20px',
+											fontSize: '16px',
+										}}
+										onClick={handleCancel}
+									>
+										<strong>BATAL</strong>
+									</Button>
+								</Grid>
+								<Grid item>
+									<Button
+										variant="contained"
+										type="submit"
+										sx={{
+											width: '160px',
+											height: '50px',
+											borderRadius: '20px',
+											fontSize: '16px',
+										}}
+									>
+										<strong>Simpan</strong>
+									</Button>
+								</Grid>
+							</Grid>
+						</Box>
+					</Paper>
+				</Box>
+			</Grid>
+		</Grid>
+	);
 }
