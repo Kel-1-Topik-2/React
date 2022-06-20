ce694b92d96a8d6a5c48dcd583b4f3c6b902c006
vidinararia <vidinararia@gmail.com>
2022-06-20 20:33:44 +0800

    add pagination in table component

--- src/component/Table/Table.jsx
+++ src/component/Table/Table.jsx
@@ -1,7 +1,24 @@
 import style from "./style.module.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactPaginate from 'react-paginate';
 
-const Table = ({ column, data, primaryKey ,aksi }) => {
+const Table = ({ column, data, primaryKey , aksi }) => {
+	const [currentItems, setCurrentItems] = useState([]);
+	const [pageCount, setPageCount] = useState(0);
+	const [itemOffset, setItemOffset] = useState(0);
+	const itemsPerPage = 10;
+
+	useEffect(() => {
+		const endOffset = itemOffset + itemsPerPage;
+		setCurrentItems(data.slice(itemOffset, endOffset));
+		setPageCount(Math.ceil(data.length / itemsPerPage));
+	}, [itemOffset, itemsPerPage, data]);
+	
+	  const handlePageClick = (event) => {
+		const newOffset = (event.selected * itemsPerPage) % data.length;
+		setItemOffset(newOffset);
+	};
+
 	return (
 		<div className={style.container}>
 			<table>
@@ -17,7 +34,7 @@ const Table = ({ column, data, primaryKey ,aksi }) => {
 					</tr>
 				</thead>
 				<tbody>
-					{data.map((row, rowIdx) => (
+					{currentItems.map((row, rowIdx) => (
 						<tr key={rowIdx}>
 							{column.map((col, colIdx) => (
 								<td key={colIdx}>{row[col.field]}</td>
@@ -42,6 +59,20 @@ const Table = ({ column, data, primaryKey ,aksi }) => {
 					))}
 				</tbody>
 			</table>
+			<ReactPaginate
+        		breakLabel="..."
+        		nextLabel=">"
+        		previousLabel="<"
+				renderOnZeroPageCount={null}
+				pageRangeDisplayed={3}
+				pageCount={pageCount}
+				onPageChange={handlePageClick}
+				containerClassName={style.pagination}
+				pageLinkClassName={style.pageNum}
+				previousLinkClassName={style.pageNum}
+				nextLinkClassName={style.pageNum}
+				activeClassName={style.active}
+      		/>
 		</div>
 	);
 };
