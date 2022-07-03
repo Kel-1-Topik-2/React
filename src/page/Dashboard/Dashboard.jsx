9f14f60eb20349a995c32af9d038f91443043b07
Dharma <dharmakirti15@gmail.com>
2022-07-04 00:22:47 +0800

    finish reslicing-dashboard

--- src/page/Dashboard/Dashboard.jsx
+++ src/page/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import axios from "../../API/api";
 import style from "./style.module.css";
@@ -12,71 +12,30 @@ import Table from "../../component/Table/Table";
 import pasien_icon from "../../assets/img/pasien_icon.svg";
 import dokter_icon from "../../assets/img/dokter_icon.svg";
 import pertemuan_icon from "../../assets/img/pertemuan_icon.svg";
-import detailIcon from "../../assets/img/detail_icon.svg";
-import deleteIcon from "../../assets/img/delete_icon.svg";
-import Modal from "../../component/Modal/Modal";
 
 const Dashboard = () => {
-  const navigate = useNavigate();
-
-  const endPoint = "pasien";
-  const [dataPasien, setDataPasien] = useState([]);
-  const [popup, setPopup] = useState({ show: false });
-  const [idPasien, setIdPasien] = useState();
+  const endPoint = "jadwal";
+  const [jadwal, setJadwal] = useState([]);
 
   useEffect(() => {
     axios.get(endPoint).then((res) => {
-      setDataPasien(res.data);
+      const newData = res.data
+
+      newData.forEach((jadwal) => {
+        jadwal.namapasien = jadwal.pasien.namapasien
+        jadwal.namadokter = jadwal.dokter.namadokter
+      })
+      
+      setJadwal(newData)
     });
   }, []);
 
-  const handleDelete = (idPasien) => {
-    setIdPasien(idPasien);
-    setPopup({
-      show: true,
-    });
-  };
-
-  const handleDeleteTrue = () => {
-    if (popup.show) {
-      axios
-        .delete(endPoint + `/${idPasien}`)
-        .then((res) => {
-          setPopup({
-            show: false,
-          });
-          navigate(0);
-        })
-        .catch((err) => console.log(err));
-    }
-  };
-
-  const handleDeleteFalse = () => {
-    setPopup({
-      show: false,
-    });
-  };
-
-  const detailClick = (id) => {
-    navigate(`/detail-data-pasien/${id}`);
-  };
-
   const column = [
-    { field: "id", header: "ID" },
-    { field: "namapasien", header: "Nama Lengkap" },
-    { field: "nik", header: "NIK" },
-    { field: "umur", header: "Usia" },
-  ];
-
-  const aksi = [
-    {
-      click: (id) => detailClick(id),
-      icon: detailIcon,
-    },
-    {
-      click: (id) => handleDelete(id),
-      icon: deleteIcon,
-    },
+    { field: "tanggal", header: "Tanggal" },
+    { field: "nourut", header: "Antrian" },
+    { field: "namapasien", header: "Nama Pasien" },
+    { field: "namadokter", header: "Nama Dokter" },
+    { field: "jp", header: "Jenis Perawatan" },
   ];
 
   return (
@@ -92,21 +51,15 @@ const Dashboard = () => {
           </div>
         </div>
 
-        <p className={style.header}>Data Pasien Terkini</p>
-        <Table
-          column={column}
-          data={dataPasien.slice(0, 9)}
-          primaryKey={"id"}
-          aksi={aksi}
-        />
-        {popup.show && (
-          <Modal
-            title={"Hapus Data Pasien"}
-            text={"Yakin untuk menghapus data pasien?"}
-            handleCancel={handleDeleteFalse}
-            handleDeleteTrue={handleDeleteTrue}
+        <div className={style.table_container}>
+          <p className={style.header}>Jadwal Pasien Hari Ini</p>
+          <Link to={"/kelola-jadwal"}>Lihat Selengkapnya</Link>
+          <Table
+            column={column}
+            data={jadwal}
+            aksi={[]}
           />
-        )}
+        </div>
       </div>
     </div>
   );
