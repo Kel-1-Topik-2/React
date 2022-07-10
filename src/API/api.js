0d29aa78bf31b0ecb613b6593d15f55fa0871d2a
Dharma <dharmakirti15@gmail.com>
2022-07-10 16:10:43 +0800

    making the bearer token on env file

--- src/API/api.js
+++ src/API/api.js
@@ -4,6 +4,6 @@ export default axios.create({
 	baseURL: 'https://springboot-postgresql-capstone.herokuapp.com/',
   headers: {
     "content-type": "application/json",
-    'Authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyIiwic3ViIjoic2FzYSJ9.7oeORU2oHYY_6ogO2Oiu0eEQ9YEmkC-mebW4HsA1qW4`
+    'Authorization': `Bearer ${process.env.REACT_APP_BEARER_TOKEN}`
   }
 });
